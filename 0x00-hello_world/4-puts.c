b5c94f518af73ff7172950fb7ae8f679919a23e3
mohamed_adel <adel762dp47@gmail.com>
2023-09-09 12:08:20 -0700

    message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
