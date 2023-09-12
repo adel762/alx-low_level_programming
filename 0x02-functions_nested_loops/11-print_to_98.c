927f36ba12e8578b8e4b8f2a8bcd6ccda7679a4c
mohamed_adel <adel762dp47@gmail.com>
2023-09-12 15:16:20 -0700

    message

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,6 +7,7 @@
  *		printed in order
  * @n: input
  */
+
 void print_to_98(int n)
 {
 	int c;
@@ -14,8 +15,9 @@ void print_to_98(int n)
 	if (n > 98)
 		for (c = n; c > 98; c--)
 			printf("%d, ", c);
-	else	
+	else
 		for (c = n; c < 98; c++)
 			printf("%d, ", c);
 	printf("98\n");
 }
+
