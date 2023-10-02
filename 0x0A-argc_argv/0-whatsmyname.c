a984f98a4e93bdd2668cd3c9fb8b1266a24c6f7b
mohamed_adel <adel762dp47@gmail.com>
2023-10-02 16:36:41 -0700

    message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ * @argc: number
+ * @argv: arr
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)aegc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
