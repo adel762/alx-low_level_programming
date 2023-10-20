8758d783946777adea6dabb9f1bc5f767259c8a8
mohamed adel <adel762dp47@gmail.com>
2023-10-19 17:28:31 -0700

    message

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *x;
+	list_t *temp;
 
 	while (head)
 	{
-		x = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = x;
+		head = temp;
 	}
 }
