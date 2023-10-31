5d57e7f294f6951d8fc5dfa46b4cf685e111c9f5
mohamed adel <adel762dp47@gmail.com>
2023-10-30 17:18:18 -0700

    message

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number
+ *
+ * Return: function
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *b;
+
+	if (filename == NULL)
+		return (0);
+
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, b, letters);
+	w = write(STDOUT_FILENO, b, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(b);
+		return (0);
+	}
+
+	free(b);
+	close(o);
+
+	return (w);
+}
